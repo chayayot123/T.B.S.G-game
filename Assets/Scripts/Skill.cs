39e38b0beeb26b6b3dd5c4638c73c5baeb3eaefc
Potchara <potchara.j@ku.th>
2023-04-12 00:55:46 +0700

    first commit

---
+++ Assets/Scripts/Skill.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Skill", menuName = "Turn-Based Game/Skill")]
+public class Skill : ScriptableObject
+{
+    public string skillName;
+    public int skillpower;
+    public string effect;
+    // public int cooldown;
+    // public int cooldowncount;
+
+    // public SkillEffect effect;
+    public Sprite icon;
+}
